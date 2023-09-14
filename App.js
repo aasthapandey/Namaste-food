a2083e056802731146add481d94dbc39e3a1b7fb
Aastha Pandey <aasthapandey@192.168.1.3>
2023-09-15 00:31:17 +0530

    Chapter02-Igniting App

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", {id: "parent"}, [React.createElement("div", {id: "child"}, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")], React.createElement("div", {id: "child"}, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]))]);
 
-const heading = React.createElement("h1", {}, "Hello world");
+const heading = React.createElement("h1", {}, "Hello world from Mini");
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
         root.render(parent);
